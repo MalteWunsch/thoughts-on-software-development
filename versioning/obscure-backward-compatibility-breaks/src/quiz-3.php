3ceaf41ece576764be897f8c4a30da5ea0a85316
Malte Wunsch <malte.wunsch@googlemail.com>
2014-05-17 21:32:25 +0200

    quiz question 3 after potential bc break and it's diff

--- versioning/obscure-backward-compatibility-breaks/src/quiz-3.php
+++ versioning/obscure-backward-compatibility-breaks/src/quiz-3.php
@@ -4,13 +4,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class MyController
 {
-    /** @Route("/second/") */
-    public function mySecondAction($requestParams) {
+    /** @Route("/{requestParams}/") */
+    public function myFirstAction($requestParams) {
         // ...
     }
 
-    /** @Route("/{requestParams}/") */
-    public function myFirstAction($requestParams) {
+    /** @Route("/second/") */
+    public function mySecondAction($requestParams) {
         // ...
     }
 }
